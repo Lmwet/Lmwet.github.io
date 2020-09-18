b0717a99f56c145bef0f2aadadb3a0eac5574359
Lmwet <lea.colson@gmail.com>
2020-09-18 12:56:40 +0200

    Updates

---
+++ precache-manifest.23e9bf5b2eefd48c71240c3949aeaa84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26b3b84d600597d04fe558a65f6c099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf59cdb605c32e48b9f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d50586d7b332205197f3",
+    "url": "/static/js/2.6826e5d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6826e5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf59cdb605c32e48b9f",
+    "url": "/static/js/main.ef3226c4.chunk.js"
+  },
+  {
+    "revision": "749ffc5d95ec7aa007d4",
+    "url": "/static/js/runtime-main.b83c4842.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
