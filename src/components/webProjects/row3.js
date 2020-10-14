5d76af11bd23ce9229ba4a1b66c4d42f0b241870
Lmwet <lea.colson@gmail.com>
2020-10-14 14:47:09 +0200

    added link to git repo

--- src/components/webProjects/row3.js
+++ src/components/webProjects/row3.js
@@ -9,12 +9,14 @@ export default function Row3() {
     altLeft: "Scratch",
     textLeft:
       "A single page app in View JS for my friends to upload and comment our hand-made drawings.",
+    gitLeft: "https://github.com/lmwet/scratch",
     urlLeft: "http://scratzch.herokuapp.com/",
     titleRight: "A Petition App for East River Park",
     imageRight: "/images/EastRiverPark.png",
     altRight: "",
     textRight:
       "A petition app in handlebars and node. Create an account, signe the petition with canvas, add your infos to a profile, unsigne the petition and check how many of us want the park to remain.",
+    gitRight: "https://github.com/lmwet/east-river-petition",
     urlRight: "http://petition-east-river-park-lmwet.herokuapp.com/",
   };
 
--- src/components/webProjects/row4.js
+++ src/components/webProjects/row4.js
@@ -2,21 +2,23 @@ import React from "react";
 import Projectrow from "../projectrow";
 
 export default function Row4() {
-    const content = {
-        sectionTitle: "PHP",
-        titleLeft: "Fairyfiles",
-        imageLeft: "/images/ff.png",
-        altLeft: "Fairyfiles-logo",
-        textLeft:
-            "A website with a very userfriendly and opensource Content Management System: Processwire. written in PHP, its API is very accessible to JQuery developpers. Illustrations and design made in collaboration with Luezlez Sol.",
-        urlLeft: "https://fairyfiles.org/",
-        titleRight: "Reproduktive Gerechtigkeit",
-        imageRight: "/images/repro.png",
-        altRight: "Reproduktive-Gerechtigkeit",
-        textRight:
-            "A wordpress website commissionned by the Heinrich Böhl Stiftung. A very slick design and based on its own wordpress theme. Logo also created by Lmwet.",
-        urlRight: "https://repro-gerechtigkeit.de/",
-    };
+  const content = {
+    sectionTitle: "PHP",
+    titleLeft: "Fairyfiles",
+    imageLeft: "/images/ff.png",
+    altLeft: "Fairyfiles-logo",
+    textLeft:
+      "A website with a very userfriendly and opensource Content Management System: Processwire. written in PHP, its API is very accessible to JQuery developpers. Illustrations and design made in collaboration with Luezlez Sol.",
+    // gitLeft: "https://github.com/lmwet/scratch",
+    urlLeft: "https://fairyfiles.org/",
+    titleRight: "Reproduktive Gerechtigkeit",
+    imageRight: "/images/repro.png",
+    altRight: "Reproduktive-Gerechtigkeit",
 
-    return <Projectrow content={content}></Projectrow>;
+    textRight:
+      "A wordpress website commissionned by the Heinrich Böhl Stiftung. A very slick design and based on its own wordpress theme. Logo also created by Lmwet.",
+    urlRight: "https://repro-gerechtigkeit.de/",
+  };
+
+  return <Projectrow content={content}></Projectrow>;
 }
